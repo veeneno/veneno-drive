550ff6745f419eb29e4ada230c639055fdbff647
Victor Almeida <155707127+veeneno@users.noreply.github.com>
2024-10-28 20:34:17 -0300

    Update bot.js
    Arquivos grandes podem ser upados/baixados agora

--- bot.js
+++ bot.js
@@ -15,6 +15,7 @@ const progressChannelId = config.progressChannelId;
 const CHUNK_SIZE = config.CHUNK_SIZE * 1024 * 1024; 
 const UPLOAD_DELAY = config.UPLOAD_DELAY;
 const MAX_RAM_USAGE = config.MAX_RAM_USAGE * 1024 * 1024 * 1024;
+const MAX_DL = config.MAX_DL;
 const embedColors = config.embedColors;
 
 const createDirectoryIfNotExists = (dirPath) => {
@@ -78,7 +79,6 @@ const showInfoEmbed = async (channel, interaction) => {
         const [name, value] = line.split(': ');
         
         if (name && value) {
-            // Campos padrão
             fields.push({ name: name.trim(), value: value.trim(), inline: false });
         } else if (line.includes('Conteúdo do Zip:')) {
             isZipContent = true;
@@ -160,8 +160,8 @@ const distributeChunksToBots = async (chunkFiles, channel, message, embed) => {
 
         for (const chunkFile of botChunks) {
             try {
-                console.log(`Bot ${i + 1} enviando chunk ${chunkFile}`);
-                
+                console.log(`Bot ${i + 1} está enviando o chunk ${chunkFile} para o canal ${channel.name}`);
+
                 if (checkMemoryUsage()) {
                     await botChannel.send({ files: [chunkFile] });
                 } else {
@@ -170,12 +170,13 @@ const distributeChunksToBots = async (chunkFiles, channel, message, embed) => {
                     fileStream.close();
                 }
 
+                fs.unlinkSync(chunkFile);  
                 chunksSent++;
                 const progressPercentage = ((chunksSent / totalChunks) * 100).toFixed(1);
                 embed.spliceFields(4, 1, { name: 'Progresso Geral', value: `${progressPercentage}% concluído` });
                 await message.edit({ embeds: [embed] });
 
-                fs.unlinkSync(chunkFile); 
+                await new Promise(resolve => setTimeout(resolve, UPLOAD_DELAY * 1000));
             } catch (error) {
                 console.error(`Erro ao bot ${i + 1} enviar chunk: ${chunkFile}`, error);
             }
@@ -225,7 +226,7 @@ const uploadFile = async (fileName, interaction, progressChannelId) => {
         .spliceFields(3, 1, { name: 'Status', value: 'Todos os arquivos foram enviados com sucesso!', inline: false });
     await message.edit({ embeds: [embed] });
 
-    const channelDir = path.join(upsDir, sanitizedChannelName);
+    const channelDir = path.join(upsDir, sanitizedChannelName.toLowerCase());
     if (!fs.existsSync(channelDir)) fs.mkdirSync(channelDir);
     fs.writeFileSync(
         path.join(channelDir, 'info.txt'),
@@ -307,45 +308,70 @@ clients[0].on('interactionCreate', async (interaction) => {
         await interaction.followUp('Upload de todos os arquivos concluído!');
     }
     else if (commandName === 'compartilhar') {
-        await interaction.deferReply(); 
+        await interaction.deferReply();
     
         const channel = interaction.options.getChannel('channel');
         if (!channel || channel.type !== ChannelType.GuildText) {
-            return await interaction.editReply('Por favor, selecione um canal de texto válido.'); 
+            return await interaction.editReply('Por favor, selecione um canal de texto válido.');
         }
     
-        const messages = await channel.messages.fetch();
-        const chunkLinks = [];
+        let chunkLinks = [];
+        let lastMessageId;
     
-        messages.forEach(msg => {
-            msg.attachments.forEach(att => {
-                if (att.name.startsWith('chunk_')) {
-                    chunkLinks.push(att.url);
-                }
+        while (true) {
+            const fetchedMessages = await channel.messages.fetch({ before: lastMessageId });
+            if (fetchedMessages.size === 0) break;
+    
+            fetchedMessages.forEach(msg => {
+                msg.attachments.forEach(att => {
+                    if (att.name.startsWith('chunk_')) {
+                        chunkLinks.push(att.url);
+                    }
+                });
             });
-        });
+    
+            if (fetchedMessages.size === 0) break; 
+            lastMessageId = fetchedMessages.last().id;
+        }
     
         if (chunkLinks.length === 0) {
-            return await interaction.editReply('Nenhum chunk encontrado no canal para compartilhar.'); 
+            return await interaction.editReply('Nenhum chunk encontrado no canal para compartilhar.');
         }
+    
+        const removerChunksDuplicados = (chunks) => {
+            const uniqueChunks = new Set();
+            return chunks.filter(chunkLink => {
+                const match = chunkLink.match(/chunk_\d+/);
+                if (match) {
+                    const chunkId = match[0]; 
+    
+                    if (uniqueChunks.has(chunkId)) {
+                        console.log(`Chunk duplicado removido: "${chunkId}"`);
+                        return false;
+                    }
+                    uniqueChunks.add(chunkId);
+                    return true; 
+                }
+                return true; 
+            });
+        };
+    
+        chunkLinks = removerChunksDuplicados(chunkLinks);
     
         const shareFilePath = path.join('./shared', `${channel.name}.txt`);
         fs.writeFileSync(shareFilePath, chunkLinks.join('\n'));
     
-        const originalFileName = channel.name; 
+        const originalFileName = channel.name;
         const chunkCount = chunkLinks.length;
-    
-        const totalBytes = config.CHUNK_SIZE * (chunkCount - 1);
-        
-        const totalSizeInGB = totalBytes / (1024 ** 3); 
+        const totalSizeInGB = ((chunkCount - 1) * CHUNK_SIZE) / (1024 ** 3); 
     
         const embed = {
-            color: 0x0099ff, 
+            color: 0x0099ff,
             title: `Compartilhamento de Chunks`,
             fields: [
                 { name: 'Nome do Arquivo Original', value: originalFileName, inline: true },
                 { name: 'Número de Chunks', value: `${chunkCount}`, inline: true },
-                { name: 'Tamanho Total (aproximado) (GB)', value: `${totalSizeInGB.toFixed(2)} GB`, inline: true }, 
+                { name: 'Tamanho Total (aproximado) (GB)', value: `${totalSizeInGB.toFixed(2)} GB`, inline: true },
             ],
             timestamp: new Date(),
             footer: {
@@ -353,8 +379,8 @@ clients[0].on('interactionCreate', async (interaction) => {
             },
         };
     
-        await interaction.editReply({ embeds: [embed] }); 
-    }
+        await interaction.editReply({ embeds: [embed] });
+    }    
     else if (commandName === 'download') {
         await interaction.deferReply();
     
@@ -369,39 +395,46 @@ clients[0].on('interactionCreate', async (interaction) => {
         if (!fs.existsSync(downloadTempDir)) fs.mkdirSync(downloadTempDir);
         if (!fs.existsSync(finalDownloadDir)) fs.mkdirSync(finalDownloadDir);
     
-        const messages = await channel.messages.fetch();
-        const chunkFiles = [];
+        let chunkFiles = [];
+        let lastMessageId;
     
-        messages.forEach(msg => {
-            msg.attachments.forEach(att => {
-                if (att.name.startsWith('chunk_')) {
-                    chunkFiles.push({ name: att.name, url: att.url });
-                }
+        while (true) {
+            const fetchedMessages = await channel.messages.fetch({ before: lastMessageId });
+            if (fetchedMessages.size === 0) break;
+    
+            fetchedMessages.forEach(msg => {
+                msg.attachments.forEach(att => {
+                    if (att.name.startsWith('chunk_')) {
+                        chunkFiles.push({ name: att.name, url: att.url });
+                    }
+                });
             });
-        });
+    
+            lastMessageId = fetchedMessages.last().id;
+        }
     
         if (chunkFiles.length === 0) {
             return await interaction.followUp('Nenhum arquivo de chunk encontrado no canal.');
         }
     
-        console.log("Iniciando o download distribuído entre bots...");
+        console.log(`Total de chunks encontrados: ${chunkFiles.length}`);
+    
         chunkFiles.sort((a, b) => {
             const aIndex = parseInt(a.name.split('_')[1]);
             const bIndex = parseInt(b.name.split('_')[1]);
             return aIndex - bIndex;
         });
     
-        const totalChunks = chunkFiles.length;
-        let chunksDownloaded = 0;
-        const chunksPerBot = Math.ceil(totalChunks / clients.length);
-    
-        const downloadColor = getRandomColor(); 
+        const downloadColor = getRandomColor();
         const downloadEmbed = new EmbedBuilder()
             .setTitle('Download em Andamento')
             .setColor(downloadColor)
             .addFields({ name: 'Progresso Geral', value: '0% concluído', inline: false });
     
         const progressMessage = await interaction.editReply({ embeds: [downloadEmbed] });
+    
+        let chunksDownloaded = 0;
+        const totalChunks = chunkFiles.length;
     
         const downloadChunksForBot = async (bot, botChunks) => {
             const botName = bot.user.tag;
@@ -429,7 +462,7 @@ clients[0].on('interactionCreate', async (interaction) => {
         };
     
         await Promise.all(clients.map((bot, i) => {
-            const botChunks = chunkFiles.slice(i * chunksPerBot, Math.min((i + 1) * chunksPerBot, totalChunks));
+            const botChunks = chunkFiles.slice(i * Math.ceil(totalChunks / clients.length), Math.min((i + 1) * Math.ceil(totalChunks / clients.length), totalChunks));
             return downloadChunksForBot(bot, botChunks);
         }));
     
@@ -440,19 +473,23 @@ clients[0].on('interactionCreate', async (interaction) => {
             const originalFileNameMatch = infoContent.match(/Nome do Arquivo: (.+)/);
             const originalFileName = originalFileNameMatch ? originalFileNameMatch[1] : 'arquivo.zip';
             const finalFilePath = path.join(finalDownloadDir, originalFileName);
+    
             const finalWriteStream = fs.createWriteStream(finalFilePath);
     
-            chunkFiles.sort((a, b) => {
-                const aIndex = parseInt(a.name.split('_')[1]);
-                const bIndex = parseInt(b.name.split('_')[1]);
-                return aIndex - bIndex;
+            finalWriteStream.on('error', (error) => {
+                console.error("Erro ao escrever o arquivo final:", error);
+                interaction.followUp("Ocorreu um erro ao unir os arquivos.");
             });
     
             for (const chunk of chunkFiles) {
                 const chunkPath = path.join(downloadTempDir, chunk.name);
                 if (fs.existsSync(chunkPath)) {
-                    const chunkData = fs.readFileSync(chunkPath);
-                    finalWriteStream.write(chunkData);
+                    const chunkReadStream = fs.createReadStream(chunkPath);
+                    await new Promise((resolve, reject) => {
+                        chunkReadStream.pipe(finalWriteStream, { end: false });
+                        chunkReadStream.on('end', resolve);
+                        chunkReadStream.on('error', reject);
+                    });
                 }
             }
     
@@ -461,9 +498,10 @@ clients[0].on('interactionCreate', async (interaction) => {
             finalWriteStream.on('finish', async () => {
                 const totalSize = chunkFiles.reduce((acc, chunk) => acc + fs.statSync(path.join(downloadTempDir, chunk.name)).size, 0);
                 const totalSizeGB = (totalSize / (1024 ** 3)).toFixed(2);
+    
                 downloadEmbed.setTitle('Download Concluído')
-                    .setDescription('Todos os chunks foram unidos!')
-                    .setColor(downloadColor) 
+                    .setDescription('Todos os chunks foram unidos com sucesso!')
+                    .setColor(downloadColor)
                     .spliceFields(0, 1, { name: 'Progresso Geral', value: '100% concluído' })
                     .addFields(
                         { name: 'Total de Chunks', value: `${chunkFiles.length}`, inline: true },
@@ -473,7 +511,6 @@ clients[0].on('interactionCreate', async (interaction) => {
                     );
     
                 chunkFiles.forEach(chunk => fs.unlinkSync(path.join(downloadTempDir, chunk.name)));
-    
                 await interaction.editReply({ embeds: [downloadEmbed] });
             });
         } else {
@@ -560,7 +597,14 @@ clients[0].on('interactionCreate', async (interaction) => {
         }
     };
 
-    await Promise.all(links.map(link => downloadLinksForBot(link)));
+    const downloadInBatches = async () => {
+        for (let i = 0; i < links.length; i += MAX_DL) {
+            const batch = links.slice(i, i + MAX_DL);
+            await Promise.all(batch.map(downloadLinksForBot));
+        }
+    };
+
+    await downloadInBatches();
 
     console.log("Todos os arquivos foram baixados. Iniciando junção...");
     
@@ -604,6 +648,7 @@ clients[0].on('interactionCreate', async (interaction) => {
         await interaction.followUp({ embeds: [downloadEmbed] });
     });
 });
+
 
 clients[0].on('interactionCreate', async (interaction) => {
     if (!interaction.isStringSelectMenu() || interaction.customId !== 'select-upload-file') return;
